653cd630995b5a7cc2e7baa85e9738eb05422800
zhangzhihua <zhangzhihua@darthcloud.com>
2021-08-29 16:01:44 +0800

    update

--- doublekit-oms-starter/src/main/java/com/doublekit/oms/config/OmsServerAutoConfiguration.java
+++ doublekit-oms-starter/src/main/java/com/doublekit/oms/config/OmsServerAutoConfiguration.java
@@ -2,17 +2,14 @@ package com.doublekit.oms.config;
 
 import com.doublekit.apibox.client.annotation.EnableApiboxClient;
 import com.doublekit.dal.starter.annotation.EnableDal;
-import com.doublekit.datafly.starter.annotation.EnableDataFly;
 import com.doublekit.dcs.starter.annotation.EnableDcs;
 import com.doublekit.dfs.starter.annotation.EnableDfs;
 import com.doublekit.dss.starter.annotation.EnableDss;
-import com.doublekit.eam.server.annotation.EnableEamServer;
 import com.doublekit.framework.starter.annotation.EnableFramework;
 import com.doublekit.iam.annotation.EnableIamServer;
 import com.doublekit.member.annotation.EnableMemberServer;
 import com.doublekit.privilege.annotation.EnablePrivilegeServer;
 import com.doublekit.product.annotation.EnableProductServer;
-import com.doublekit.rpc.starter.annotation.EnableRpcClient;
 import com.doublekit.tenant.annotation.EnableTenantServer;
 import com.doublekit.toolkit.annotation.EnableToolkitServer;
 import com.doublekit.trade.annotation.EnableTradeServer;
@@ -31,13 +28,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableDfs
 @EnableDcs
 @EnableDss
-@EnableRpcClient
-@EnableDataFly
 //modules
 @EnableUserServer
 @EnablePrivilegeServer
 @EnableToolkitServer
-@EnableEamServer
 //---------
 @EnableMemberServer
 @EnableIamServer
