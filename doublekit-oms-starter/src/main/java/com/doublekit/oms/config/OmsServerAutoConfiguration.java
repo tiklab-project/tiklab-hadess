55df414e271d45ebd1a26d6fea79409a3352e918
li'mign'liang <lmingl@yeah.net>
2021-12-07 18:03:04 +0800

    oms

--- doublekit-oms-starter/src/main/java/com/doublekit/oms/config/OmsServerAutoConfiguration.java
+++ doublekit-oms-starter/src/main/java/com/doublekit/oms/config/OmsServerAutoConfiguration.java
@@ -1,18 +1,22 @@
 package com.doublekit.oms.config;
 
 import com.doublekit.apibox.client.annotation.EnableApiboxClient;
+import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 import com.doublekit.dcs.starter.annotation.EnableDcs;
 import com.doublekit.dfs.starter.annotation.EnableDfs;
+import com.doublekit.dsl.starter.annotation.EnableDsl;
 import com.doublekit.dss.starter.annotation.EnableDss;
-import com.doublekit.framework.starter.annotation.EnableFramework;
-import com.doublekit.iam.annotation.EnableIamServer;
+
+import com.doublekit.eam.server.annotation.EnableEamServer;
+import com.doublekit.iam.server.annotation.EnableIamServer;
+import com.doublekit.licence.annotation.EnableLicenceServer;
 import com.doublekit.member.annotation.EnableMemberServer;
 import com.doublekit.privilege.annotation.EnablePrivilegeServer;
 import com.doublekit.product.annotation.EnableProductServer;
-import com.doublekit.tenant.annotation.EnableTenantServer;
+import com.doublekit.subscribe.annotation.EnableSubscribeServer;
+import com.doublekit.tenant.server.annotation.EnableTenantServer;
 import com.doublekit.toolkit.annotation.EnableToolkitServer;
-import com.doublekit.trade.annotation.EnableTradeServer;
 import com.doublekit.user.annotation.EnableUserServer;
 import com.doublekit.web.starter.annotation.EnableWeb;
 import org.springframework.context.annotation.Configuration;
@@ -22,22 +26,27 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 //common
-@EnableFramework
+//@EnableFramework
+@EnableBeans
 @EnableWeb
 @EnableDal
 @EnableDfs
 @EnableDcs
+@EnableDsl
 @EnableDss
 //modules
 @EnableUserServer
+@EnableEamServer
 @EnablePrivilegeServer
 @EnableToolkitServer
-//---------
+//ocs
 @EnableMemberServer
 @EnableIamServer
 @EnableTenantServer
 @EnableProductServer
-@EnableTradeServer
+@EnableSubscribeServer
+@EnableLicenceServer
+//@EnableTradeServer
 @EnableApiboxClient
 public class OmsServerAutoConfiguration {
 }
