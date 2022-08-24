3c47e14206a8b5ac391dddcbdad46a52eda9ba4d
limingliang <darth2020>
2022-08-24 09:02:56 +0800

    调整

--- tiklab-oms-starter/src/main/java/com/tiklab/oms/OmsAutoConfiguration.java
+++ tiklab-oms-starter/src/main/java/com/tiklab/oms/OmsAutoConfiguration.java
@@ -10,6 +10,7 @@ import com.tiklab.dss.starter.EnableDss;
 import com.tiklab.eam.starter.EnableEam;
 import com.tiklab.gateway.starter.EnableGateway;
 import com.tiklab.join.starter.annotation.EnableJoin;
+import com.tiklab.licence.starter.EnableLicenceServer;
 import com.tiklab.message.starter.EnableMessage;
 import com.tiklab.oms.annotation.EnableOmsServer;
 //import com.tiklab.privilege.EnablePrivilegeServer;
@@ -35,6 +36,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableRpc
 @EnableUser
 @EnableEam
+@EnableLicenceServer
 //@EnablePrivilegeServer
 //@EnableToolkitServer
 //ocs
