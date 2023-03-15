427abf945b1c39082b742204b113bd82d8d01de8
limingliang <darth2020>
2023-03-15 20:41:09 +0800

    静态文件修改

--- tiklab-xpack-starter/bin/startup.sh
+++ tiklab-xpack-starter/bin/startup.sh
@@ -5,7 +5,7 @@
 #-------------------------------------------------------------------------------------------------------------
 DIRS=$(dirname "$PWD")
 
-APP_MAIN="net.tiklab.xpack.XpackApplication"
+APP_MAIN="io.tiklab.xpack.XpackApplication"
 JAVA_HOME="/usr/local/jdk-16.0.2"
 
 JDK_VERSION=jdk-16.0.2
