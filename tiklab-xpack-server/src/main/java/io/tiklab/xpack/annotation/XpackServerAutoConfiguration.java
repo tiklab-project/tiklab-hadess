f8bf86d2f71460feafe395771f2fa4274352f215
limingliang <darth2020>
2023-03-15 20:36:20 +0800

    静态文件修改

--- tiklab-xpack-server/src/main/java/io/tiklab/xpack/annotation/XpackServerAutoConfiguration.java
+++ tiklab-xpack-server/src/main/java/io/tiklab/xpack/annotation/XpackServerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package io.tiklab.xpack.annotation;
 
-import  io.tiklab.dsm.annotation.SQL;
+import io.tiklab.dsm.model.SQL;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +9,14 @@ import org.springframework.context.annotation.Configuration;
  * OmsServerAutoConfiguration
  */
 @Configuration
-@SQL(modules = {"xpack"})
 @ComponentScan({"io.tiklab.xpack"})
 public class XpackServerAutoConfiguration {
+
+    @Bean
+    SQL xpackSQL(){
+        return new SQL(new String[]{
+                "xpack"
+        },101);
+    }
 }
 
