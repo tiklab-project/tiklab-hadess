33ef8b1b44eff1eb3293cb70c657dbfd9768bfbb
limingliang <darth2020>
2023-05-16 17:53:29 +0800

    oplog_1.0.2_dml 提交

--- tiklab-xpack-starter/src/main/java/io/tiklab/xpack/XpackSqlLoad.java
+++ tiklab-xpack-starter/src/main/java/io/tiklab/xpack/XpackSqlLoad.java
@@ -43,6 +43,10 @@ public class XpackSqlLoad {
                 "user_1.0.1_dml",
                 "oplog_1.0.1_dml"
         });
+
+        dsmConfig.newVersion("1.0.2", new String[]{
+                "oplog_1.0.2_dml"
+        });
         return dsmConfig;
     }
 }
