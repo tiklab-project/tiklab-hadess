d8d7636c6c9a4575bfe99f9d1f5411eaa4bf640a
limingliang <darth2020>
2023-07-14 09:27:24 +0800

    测试

--- tiklab-xpack-server/src/test/java/io/tiklab/xpack/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package io.tiklab.xpack.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@Import(UserServerAutoConfiguration.class)
-public class TestConfig {
-}
-
